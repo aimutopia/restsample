ed9930a390cbcf505ab50d55b703f7b74a13adf9
Shivji Kumar Jha <shivjikumar.jha@swiggy.in>
2017-12-03 22:13:43 +0530

    first commit with a sample java+maven rest project
    Sample code based on
    http://www.springboottutorial.com/creating-rest-service-with-spring-boot

---
+++ src/main/java/com/aimutopia/bakar/BakarApplication.java
@@ -0,0 +1,12 @@
+package com.aimutopia.bakar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//public class BakarApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(BakarApplication.class, args);
+//	}
+//}
